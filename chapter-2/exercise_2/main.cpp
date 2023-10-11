35e66a913ced85761b9177862aa387da71bd5529
kaba64 <Kazem Bazesefidpar>
2023-10-11 12:20:05 +0200

    The exercises 1 and 2 done!

---
+++ chapter-2/exercise_2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::cout<<"Is there a bug here?"<<std::endl;
+  std::cout<<"Yes, there was. It has been fixed now!"<<std::endl;
+  return 0;
+}
