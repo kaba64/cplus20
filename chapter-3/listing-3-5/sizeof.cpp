62c6eb00e61dec8f7b0854fbd14aea50a1ed5f00
kaba64 <Kazem Bazesefidpar>
2023-10-27 17:58:40 +0200

    Testing <cstdint> for fixed size integer!

--- chapter-3/listing-3-5/sizeof.cpp
+++ chapter-3/listing-3-5/sizeof.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 
 auto main() -> int
 {
@@ -6,4 +7,6 @@ auto main() -> int
   std::cout<<"unsigned int = "<<sizeof(unsigned int)<<"\n";
   std::cout<<"float = "<<sizeof(float)<<"\n";
   std::cout<<"double = "<<sizeof(double)<<"\n";
+  std::cout<<"int 32-bit = "<<sizeof(int32_t)<<"\n";
+  std::cout<<"int 64-bit = "<<sizeof(int64_t)<<"\n";
 }
