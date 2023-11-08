e3fb01b998bf82a10e736fd7c174141cedbc1490
kaba64 <Kazem Bazesefidpar>
2023-11-08 18:42:42 +0100

    exercise_2 is chapter 3!

---
+++ chapter-3/exercise_2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(void){
+  std::cout<<"The size of unsigned integer in byte is "<<sizeof(unsigned int)<<std::endl;
+  std::cout<<"The size of integer in byte is "<<sizeof(int)<<std::endl;
+  std::cout<<"The size of long integer in byte is "<<sizeof(long int)<<std::endl;
+}
