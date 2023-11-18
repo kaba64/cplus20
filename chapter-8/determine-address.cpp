d7c77f82e00d774910277949f52b4f22812cef8c
kaba64 <Kazem Bazesefidpar>
2023-11-18 19:52:00 +0100

    Cleaned!

--- chapter-8/determine-address.cpp
+++ chapter-8/determine-address.cpp
@@ -4,7 +4,7 @@ auto main() -> int
 {
   int var1{1};
   double var2{3.};
-  std::cout<<"Address of the int variable : "<<std::hex<<var1<<"\n";
+  std::cout<<"Address of the int variable : "<<var1<<"\n";
   std::cout<<"Address of the int variable : "<<&var1<<"\n";
   std::cout<<"Address of the double variable : "<<&var2<<"\n";
 }
